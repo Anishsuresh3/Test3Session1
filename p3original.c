0bd9dc810f055c2c280b61bd76546a60022eff12
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-03-06 15:46:21 +0000

    ..

--- p3original.c
+++ p3original.c
@@ -4,12 +4,29 @@ void input_n_and_r(int *n, int *r)
   printf("Enter the value of n and r\n");
   scanf("%d%d",n,r);  
 }
+//recursive method
 int ncr(int n, int r)
 {
   if(r==0 || n==r)
     return 1;
   else
     return ncr(n-1,r-1)+ncr(n-1,r);
+}
+//iterative method
+int fact(int n)
+{
+  int f=1;
+  for(int i=2;i<=n;i++)
+     f=f*i;
+  return f;
+} 
+int ncr1(int n, int r)
+{
+  int t1,t2,t3;
+  t1=fact(n);
+  t2=fact(r);
+  t3=fact(n-r);
+  return t1/(t2*t3);
 }
 void output(int n, int r, int result)
 {
